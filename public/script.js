330e40c917f1eecf360139fe7b7c5f41c9a86f4d
kyonori1 <reustache2019@bklawtech.com>
2017-10-30 22:32:15 +0000

    second edit

--- public/script.js
+++
@@ -1,8 +0,0 @@
-//Unit 1 Portfolio with Firebase
-// Use Firebase to store the images links and info for your projects. 
-// Use Jquery to dynamically display the information from firebse. 
-
-// *************************** YOUR CODE BELOW *******************************
-//******************TEST EARLY AND OFTEN USING console.log() ******************
-//****************** SERIOUSLY TEST USING console.log()!!! ******************
-
